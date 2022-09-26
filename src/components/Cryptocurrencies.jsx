e7a1ba01ee918fb030e095b9581948a011709dd4
colonelxy <harold2@protonmail.com>
2022-09-27 02:24:51 +0300

    news part one

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import millify from 'millify';
 import { Link } from 'react-router-dom';
-import { Card, Row, Col, Input} from 'antd';
+import { Card, Row, Col} from 'antd';
 
 import { useGetCryptosQuery } from '../services/cryptoApi';
 
 const Cryptocurrencies = ({simplified}) => {
   const count = simplified ? 10 : 100;
-  const [ data: cryptosList, isFetching ] = useGetCryptosQuery(count);
+  const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
   const [cryptos, setCryptos] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
 
@@ -34,7 +34,7 @@ const Cryptocurrencies = ({simplified}) => {
             <Link to={`/crypto/${currency.id}`} >
               <Card 
                 title={`${currency.rank}, ${currency.name}`}
-                extra={<img className='crypto-img' src={currency.iconUrl} />}
+                extra={<img className='crypto-img' src={currency.iconUrl} alt='' />}
                 hoverable
                  >
                   <p>Price: {millify(currency.price)}</p>
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React from 'react';
+import { Select, Typography, Row, Column, Avatar, Card } from 'antd';
+import moment from 'moment';
+
+import { useGetCryptoNewsQuery } from "../services/cryptoNewsApi";
+
+const { Text, Title } = Typography;
+const { Option } = Select;
 
 const News = () => {
+  const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: 'Cryptocurrency', count: simplified ? 6 : 12 })
+
+  if(!cryptoNews?.value) return 'Loading...';
   return (
-    <div>News</div>
+    <Row gutter={[24,24]} >
+      {cryptoNews.value.map((news,i) => {
+        <Col xs={24} sm={12} lg={8} key={i} >
+          <Card hoverable className='news-card' >
+            <a href = {news.url} target='_blank' rel="noreferrer" >
+              <div className='news-image-container' >
+                <Title className='news-title' level={4} >{news.name}</Title>
+
+              </div>
+
+            </a>
+
+          </Card>
+        </Col>
+
+      })}
+
+    </Row>
   )
 }
 
